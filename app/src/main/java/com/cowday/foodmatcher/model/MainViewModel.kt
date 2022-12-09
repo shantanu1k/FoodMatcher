a711106ef2926a1768906aee97d2ac535901f369
shantanu1k <shantanukawade2@gmail.com>
2022-12-09 12:07:13 +0530

    viewmodel setup

--- app/src/main/java/com/cowday/foodmatcher/model/MainViewModel.kt
+++ app/src/main/java/com/cowday/foodmatcher/model/MainViewModel.kt
@@ -1,4 +1,19 @@
 package com.cowday.foodmatcher.model
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.cowday.foodmatcher.data.BeerItem
+import com.cowday.foodmatcher.repository.PunkRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(private val repository: PunkRepository): ViewModel() {
+    private val _beers: MutableLiveData<List<BeerItem>> = MutableLiveData()
+    val beers: LiveData<List<BeerItem>>
+        get() = _beers
+    suspend fun getDataFromNetwork(){
+        _beers.value = repository.getDataFromNetwork()
+    }
 }
