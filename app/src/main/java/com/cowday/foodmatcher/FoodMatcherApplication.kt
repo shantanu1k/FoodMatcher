c47866f7b85c6fab02487aea79b27386f152ab98
shantanu1k <shantanukawade2@gmail.com>
2022-12-09 12:02:49 +0530

    adds the application class

--- app/src/main/java/com/cowday/foodmatcher/FoodMatcherApplication.kt
+++ app/src/main/java/com/cowday/foodmatcher/FoodMatcherApplication.kt
@@ -1,4 +1,8 @@
 package com.cowday.foodmatcher
 
-class FoodMatcherApplication {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FoodMatcherApplication: Application() {
 }
