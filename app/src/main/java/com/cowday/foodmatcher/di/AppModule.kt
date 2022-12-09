6fe5b909a22a039f2bbcd0b21c32f88fc818b7c6
shantanu1k <shantanukawade2@gmail.com>
2022-12-09 11:57:55 +0530

    adds the dependencies

--- app/src/main/java/com/cowday/foodmatcher/di/AppModule.kt
+++ app/src/main/java/com/cowday/foodmatcher/di/AppModule.kt
@@ -1,4 +1,26 @@
 package com.cowday.foodmatcher.di
 
+import com.cowday.foodmatcher.network.PunkApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
+    @Provides
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(PunkApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideApi(retrofit: Retrofit): PunkApi{
+        return retrofit.create(PunkApi::class.java)
+    }
 }
