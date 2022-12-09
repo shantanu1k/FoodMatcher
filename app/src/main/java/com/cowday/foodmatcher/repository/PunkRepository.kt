623bbc7296e6d8acb43c8c4eeb2eb120a964a273
shantanu1k <shantanukawade2@gmail.com>
2022-12-09 12:05:07 +0530

    adds network calls to api

--- app/src/main/java/com/cowday/foodmatcher/repository/PunkRepository.kt
+++ app/src/main/java/com/cowday/foodmatcher/repository/PunkRepository.kt
@@ -1,4 +1,29 @@
 package com.cowday.foodmatcher.repository
 
-class PunkRepository {
+import android.util.Log
+import com.cowday.foodmatcher.data.Beer
+import com.cowday.foodmatcher.data.BeerItem
+import com.cowday.foodmatcher.network.PunkApi
+import retrofit2.HttpException
+import retrofit2.Response
+import retrofit2.Retrofit
+import java.io.IOException
+import javax.inject.Inject
+
+class PunkRepository @Inject constructor(private val punkApi: PunkApi) {
+    suspend fun getDataFromNetwork(): List<BeerItem>{
+        val response: Response<List<BeerItem>> = try{
+            punkApi.getBeers()
+        } catch (e: HttpException){
+            Log.d("Exception", "HTTP")
+            return emptyList()
+        } catch (e: IOException){
+            Log.d("Exception", "IO")
+            return emptyList()
+        }
+        if(response.isSuccessful && response.body() != null){
+            return response.body()!!
+        }
+        return emptyList()
+    }
 }
