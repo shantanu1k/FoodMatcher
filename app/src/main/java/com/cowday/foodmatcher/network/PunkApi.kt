0e8e7a5f2ca73b1f863700c628605daa055cfc4f
shantanu1k <shantanukawade2@gmail.com>
2022-12-09 15:55:19 +0530

    adds the get request according to food

--- app/src/main/java/com/cowday/foodmatcher/network/PunkApi.kt
+++ app/src/main/java/com/cowday/foodmatcher/network/PunkApi.kt
@@ -3,13 +3,17 @@ package com.cowday.foodmatcher.network
 import com.cowday.foodmatcher.data.BeerItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PunkApi {
     companion object{
         const val BASE_URL = "https://api.punkapi.com/v2/"
     }
 
-    @GET("beers?page=2&per_page=10")
+    @GET("beers?page=1&per_page=10")
     suspend fun getBeers(): Response<List<BeerItem>>
 
+    @GET("beers")
+    suspend fun getBeersForFood(@Query("food") foodName: String): Response<List<BeerItem>>
 }
