a1c13154ed48c3664e8e8c79d01a78b7ee9d4d0b
shantanu1k <shantanukawade2@gmail.com>
2022-12-10 17:42:33 +0530

    removes the TypeConverters

--- app/src/main/java/com/cowday/foodmatcher/database/BeerDatabase.kt
+++ app/src/main/java/com/cowday/foodmatcher/database/BeerDatabase.kt
@@ -5,10 +5,8 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.cowday.foodmatcher.data.BeerItem
 import com.cowday.foodmatcher.data.SimpleBeerItem
-import com.cowday.foodmatcher.utils.StringConverters
 
 @Database(entities = [SimpleBeerItem::class], exportSchema = false, version = 2)
-@TypeConverters(StringConverters::class)
 abstract class BeerDatabase: RoomDatabase() {
     abstract fun beerDao(): BeerDao
 }
