466709bc4f151d3ebfd0e4101628ad22d30f873a
shantanu1k <shantanukawade2@gmail.com>
2022-12-10 17:38:09 +0530

    adds the dependency for the database

--- app/src/main/java/com/cowday/foodmatcher/di/AppModule.kt
+++ app/src/main/java/com/cowday/foodmatcher/di/AppModule.kt
@@ -1,16 +1,23 @@
 package com.cowday.foodmatcher.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.cowday.foodmatcher.database.BeerDatabase
 import com.cowday.foodmatcher.network.PunkApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+    @Singleton
     @Provides
     fun provideRetrofit(): Retrofit{
         return Retrofit.Builder()
@@ -19,8 +26,17 @@ object AppModule {
             .build()
     }
 
+    @Singleton
     @Provides
     fun provideApi(retrofit: Retrofit): PunkApi{
         return retrofit.create(PunkApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext app: Context): BeerDatabase{
+        return Room.databaseBuilder(app, BeerDatabase::class.java, "beer_database")
+            .build()
+    }
+
 }
