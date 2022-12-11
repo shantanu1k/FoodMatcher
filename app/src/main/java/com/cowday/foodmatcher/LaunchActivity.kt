ba638e287cf64520135f1107193fd185fe6b9eaa
shantanu1k <shantanukawade2@gmail.com>
2022-12-11 12:13:33 +0530

    adds the new launch activity

---
+++ app/src/main/java/com/cowday/foodmatcher/LaunchActivity.kt
@@ -0,0 +1,14 @@
+package com.cowday.foodmatcher
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
