5d0b80b992484c672b88f16a68606b106cfdf919
shantanu1k <shantanukawade2@gmail.com>
2022-12-11 16:11:50 +0530

    removes the unused imports

--- app/src/main/java/com/cowday/foodmatcher/database/BeerDao.kt
+++ app/src/main/java/com/cowday/foodmatcher/database/BeerDao.kt
@@ -1,6 +1,5 @@
 package com.cowday.foodmatcher.database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.IGNORE
--- app/src/main/java/com/cowday/foodmatcher/database/BeerDatabase.kt
+++ app/src/main/java/com/cowday/foodmatcher/database/BeerDatabase.kt
@@ -2,8 +2,6 @@ package com.cowday.foodmatcher.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.cowday.foodmatcher.data.BeerItem
 import com.cowday.foodmatcher.data.SimpleBeerItem
 
 @Database(entities = [SimpleBeerItem::class], exportSchema = false, version = 2)
